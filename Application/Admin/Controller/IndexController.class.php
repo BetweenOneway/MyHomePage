701a9079119bbba857be059b0afb053b978b5805
王者伟业 <wang1902568721@163.com>
2020-04-29 00:26:00 +0800

    implement change password front end algorithm

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -6,12 +6,21 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $this->display();
+        $this->display("Index:index");
     }
 
     public function changePassword()
     {
-        $this->display();
+        if(IS_POST)
+        {
+            $res['status']=0;
+            $res['message']="调用changePassword";
+            $this->ajaxReturn($res);
+        }
+        else
+        {
+            $this->display();
+        }
     }
 }
 
