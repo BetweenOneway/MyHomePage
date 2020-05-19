c755f6d97aa7df1a77b8b48cc8291425ff6dc898
王者伟业 <wang1902568721@163.com>
2020-05-19 21:06:40 +0800

    implement hot function module

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,3 +1,11 @@
 <?php
 
-?>
+function get_cover_url($picture)
+{
+    if($picture){
+        $url = __ROOT__.'/Uploads/'.$picture;
+    }else{
+        $url = __ROOT__.'/Uploads/photo.jpg';
+    }
+    return $url;
+}
