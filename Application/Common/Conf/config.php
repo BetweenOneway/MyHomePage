623e9f2fd1f149dd487c378c91f86110d7a801cf
wei wang <wei.wang@workplace.com>
2021-05-08 20:20:10 +0800

    add header part for Note

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,8 +3,8 @@ return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'127.0.0.1',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'root',
+	'DB_USER'=>'navigation',
+	'DB_PWD'=>'5GiCAFrtfRENrAaN',
 	'DB_NAME'=>'navigation',
 	'DB_PREFIX'=>'nav_',
 );
