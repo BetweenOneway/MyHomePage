bbb083384c39259db4b4e9624042e18ad6bf527d
wei wang <weiwang@fedora.com>
2021-08-26 22:30:01 +0800

    add user login check

--- Application/Note/Controller/IndexController.class.php
+++ Application/Note/Controller/IndexController.class.php
@@ -1,7 +1,19 @@
 <?php
 namespace Note\Controller;
 use Think\Controller;
+
 class IndexController extends Controller {
+    //判断是否登录
+    public function _initialize()
+    {
+        $admin_id = session('admin_id');
+        $admin_username = session('admin_username');
+        if(!isset($admin_id) || !isset($admin_username))
+        {
+            $this->redirect('Admin/Public/login', 5,'你尚未登录，跳转到登录页面。。。');
+        }
+    }
+
     public function index(){
         $this->display();
     }
